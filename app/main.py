d0046e22b794f2e7a15894b5b45f428f85938c55
joosthb <joost@hendricksen.net>
2023-08-09 14:58:43 +0200

    added CRUD

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request, Form
-
 from typing import Annotated
-
 from fastapi.templating import Jinja2Templates
 from app.model import wpa_psk
+
+import schemas
+
+data = []
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
@@ -12,20 +14,35 @@ templates = Jinja2Templates(directory="templates/")
 async def root():
     return {"message": "Hello World!"}
 
-@app.get("/connections/{connection_id}")
-async def read_connection(connection_id: int):
-    return {"connection_id": connection_id}
+@app.post("/connection")
+def add_connection(connection: Connection):
+   data.append(connection.dict())
+   return data
 
-@app.get("/form")
-def form_post(request: Request):
-    result = "Type a number"
-    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+@app.get("/connections")
+def get_connections():
+   return data
 
-@app.post("/form")
-async def gen_psk(request: Request, ssid: Annotated[str, Form()], password: Annotated[str, Form()]):
-    result = wpa_psk(ssid, password)
-    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+@app.get("/connection/{id}")
+def get_connection(id: int):
+   return data[id]
+
+@app.delete("/connection/{id}")
+def delete_connection(id: int):
+   data.pop(id)
+   return data
 
 @app.get("/wpa_supplicant.conf")
 def get_wpa_sup(request: Request):
-    return templates.TemplateResponse('wpa_supplicant.conf', context={'request': request, 'networks': [{'ssid': 'ssid1', 'psk': 'psk1'}, {'ssid': 'ssid2', 'psk': 'psk2'}]})
+    return templates.TemplateResponse('wpa_supplicant.conf', context={'request': request, 'networks': data})
+
+# @app.get("/form")
+# def form_post(request: Request):
+#     result = "Type a number"
+#     return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
+# @app.post("/form")
+# async def gen_psk(request: Request, ssid: Annotated[str, Form()], password: Annotated[str, Form()]):
+#     result = wpa_psk(ssid, password)
+#     return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
