59392f8ddc070ad78100df1d936cc9d993e80c95
joosthb <joost@hendricksen.net>
2023-08-09 17:15:59 +0200

    created GUI 1

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ from fastapi.templating import Jinja2Templates
 
 from app.wpa_psk import wpa_psk
 from app import models, schemas
+
 from app.database import Base, engine, SessionLocal
 
 from sqlalchemy.orm import Session
+
 #This will create our database if it doesent already exists
 Base.metadata.create_all(engine)
 def get_session():
@@ -20,8 +22,9 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+def root(request: Request, session: Session = Depends(get_session)):
+    connections = session.query(models.Connection).all()
+    return templates.TemplateResponse('connections.html', context={'request': request, 'connections': connections})
 
 @app.post("/connection")
 def createConnection(connection: schemas.Connection, session = Depends(get_session)):
@@ -44,11 +47,6 @@ def updateConnection(id:int, connection:schemas.Connection, session = Depends(ge
     session.commit()
     return itemObject
 
-# @app.delete("/connection/{id}")
-# def deleteConnection(id: int):
-#    data.pop(id)
-#    return data
-
 @app.delete("/connection/{id}")
 def deleteItem(id:int, session = Depends(get_session)):
     itemObject = session.query(models.Connection).get(id)
@@ -62,14 +60,10 @@ def getConnections(session: Session = Depends(get_session)):
     connections = session.query(models.Connection).all()
     return connections
 
-# @app.get("/wpa_supplicant.conf")
-# def get_wpa_sup(request: Request):
-#     return templates.TemplateResponse('wpa_supplicant.conf', context={'request': request, 'networks': data})
-
 @app.get("/wpa_supplicant.conf")
 def get_wpa_sup(request: Request, session: Session = Depends(get_session)):
     connections = session.query(models.Connection).all()
-    return templates.TemplateResponse('wpa_supplicant.conf', context={'request': request, 'networks': connections})
+    return templates.TemplateResponse('wpa_supplicant.conf', context={'request': request, 'connections': connections})
 
 # @app.get("/form")
 # def form_post(request: Request):
@@ -80,4 +74,3 @@ def get_wpa_sup(request: Request, session: Session = Depends(get_session)):
 # async def gen_psk(request: Request, ssid: Annotated[str, Form()], password: Annotated[str, Form()]):
 #     result = wpa_psk(ssid, password)
 #     return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
-
